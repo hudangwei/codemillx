9f8c99bd2afc9adbc261ebb4627c713247e8d603
hudangwei <13131889@qq.com>
2022-03-19 11:54:53 +0800

    支持buildFlags

--- cmd/codemillx/main.go
+++ cmd/codemillx/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
 
 	"github.com/hudangwei/codemillx"
 	"github.com/hudangwei/codemillx/codemill"
@@ -46,56 +47,12 @@ func main() {
 type BuildFlags []string
 
 func (v *BuildFlags) Set(s string) error {
-	var err error
-	*v, err = splitQuotedFields(s)
-	if *v == nil {
-		*v = []string{}
-	}
-	return err
+	*v = strings.Split(s, ",")
+	return nil
 }
 
 func (v *BuildFlags) Get() interface{} { return *v }
 
-func splitQuotedFields(s string) ([]string, error) {
-	// Split fields allowing '' or "" around elements.
-	// Quotes further inside the string do not count.
-	var f []string
-	for len(s) > 0 {
-		for len(s) > 0 && isSpaceByte(s[0]) {
-			s = s[1:]
-		}
-		if len(s) == 0 {
-			break
-		}
-		// Accepted quoted string. No unescaping inside.
-		if s[0] == '"' || s[0] == '\'' {
-			quote := s[0]
-			s = s[1:]
-			i := 0
-			for i < len(s) && s[i] != quote {
-				i++
-			}
-			if i >= len(s) {
-				return nil, fmt.Errorf("unterminated %c string", quote)
-			}
-			f = append(f, s[:i])
-			s = s[i+1:]
-			continue
-		}
-		i := 0
-		for i < len(s) && !isSpaceByte(s[i]) {
-			i++
-		}
-		f = append(f, s[:i])
-		s = s[i:]
-	}
-	return f, nil
-}
-
 func (v *BuildFlags) String() string {
 	return "<BuildFlags>"
-}
-
-func isSpaceByte(c byte) bool {
-	return c == ' ' || c == '\t' || c == '\n' || c == '\r'
 }
